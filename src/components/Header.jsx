a406b43e9a416747630b294f5809f58bd4132bda
Sanjna Srivastava <sanjnasrivastava.1610@gmail.com>
2024-06-10 20:03:10 +0530

    GTM addded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,16 @@
 import { ContactHeader } from '@/components/ContactHeader'
 import { Navbar } from '@/components/Navbar'
 import { usePathname } from 'next/navigation'
+import { GoogleTag } from '@/components/GoogleTag'
 import clsx from 'clsx'
-
 export function Header() {
   const pathname = usePathname()
+
   return (
     <header
       className={clsx(pathname === '/parents' ? 'bg-white' : 'bg-purple-25')}
     >
+      <GoogleTag />
       <ContactHeader pathname={pathname} />
       <Navbar />
     </header>
